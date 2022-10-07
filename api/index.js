6c7b419126e1347783e37d6fe669a22fe396f032
Nargiza Sainazarova <nargizasaina@gmail.com>
2022-10-07 14:40:31 +0600

    Made connection and disconnection to WS

--- api/index.js
+++ api/index.js
@@ -8,6 +8,19 @@ const port = 8000;
 
 app.use(cors());
 
+const activeConnections = {};
+
+app.ws('/canvas', (ws, req) => {
+    const id = nanoid();
+    activeConnections[id] = ws;
+    console.log('conn');
+
+    ws.on('close', () => {
+        console.log('Client disconnected! id=', id);
+        delete activeConnections[id];
+    });
+});
+
 app.listen(port, () => {
     console.log('Server started on port ', port);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 const App = () => {
+    const [state, setState] = useState({
+        mouseDown: false,
+        pixelsArray: []
+    });
+
+    const canvas = useRef(null);
+
+    useEffect(() => {
+        canvas.current = new WebSocket('ws://localhost:8000/canvas');
+
+    });
+
+    const canvasMouseMoveHandler = event => {
+        if (state.mouseDown) {
+            const clientX = event.clientX;
+            const clientY = event.clientY;
+            setState(prevState => {
+                return {
+                    ...prevState,
+                    pixelsArray: [...prevState.pixelsArray, {
+                        x: clientX,
+                        y: clientY
+                    }]
+                };
+            });
+
+            const context = canvas.current.getContext('2d');
+            // context.scale(2, 2);
+            // context.lineCap = 'round';
+            // context.lineWidth = 5;
+            const imageData = context.createImageData(10, 10);
+            const d = imageData.data;
+
+            d[0] = 0;
+            d[1] = 0;
+            d[2] = 0;
+            d[3] = 255;
+
+            context.putImageData(imageData, event.clientX, event.clientY);
+        }
+    };
+
+    const mouseDownHandler = event => {
+        setState({...state, mouseDown: true});
+    };
+
+    const mouseUpHandler = event => {
+        // Где-то здесь отправлять массив пикселей на сервер
+        setState({...state, mouseDown: false, pixelsArray: []});
+    };
+
+    const onChange = e => {
+        console.log(e.target.value);
+    };
+
     return (
         <div>
-          
+            <canvas
+                ref={canvas}
+                style={{border: '1px solid black'}}
+                width={800}
+                height={600}
+                onMouseDown={mouseDownHandler}
+                onMouseUp={mouseUpHandler}
+                onMouseMove={canvasMouseMoveHandler}
+            />
+            <input style={{margin: '20px 0'}} type="color" onChange={onChange}/>
         </div>
     );
 };
